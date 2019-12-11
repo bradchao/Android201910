2f0710a64d057f01158dc176bf70eb2ee0ad60e3
brad <brad@brad.tw>
2019-12-11 19:48:04 +0800

    2

--- app/src/main/java/tw/org/iii/android201910/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201910/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -14,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
+        initWebView();
+    }
+
+    private void initWebView(){
+        WebViewClient client = new WebViewClient();
+        webView.setWebViewClient(client);
+
+        webView.loadUrl("https://www.iii.org.tw");
     }
 }
