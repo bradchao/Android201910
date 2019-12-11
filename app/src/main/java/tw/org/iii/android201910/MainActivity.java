e9375045ba249bcaacae54d075a3fb52c12a0606
brad <brad@brad.tw>
2019-12-11 20:51:21 +0800

    8

--- app/src/main/java/tw/org/iii/android201910/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201910/MainActivity.java
@@ -3,18 +3,24 @@ package tw.org.iii.android201910;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private EditText lottery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lottery = findViewById(R.id.lottery);
         webView = findViewById(R.id.webView);
         initWebView();
     }
@@ -36,5 +42,17 @@ public class MainActivity extends AppCompatActivity {
         }else {
             super.onBackPressed();
         }
+    }
+
+    public class MyBrad {
+        @JavascriptInterface
+        public void callFramJS() {
+            Log.v("brad", "i got it");
+        }
+    }
+
+
+    public void test1(View view) {
+        webView.loadUrl("javascript:doHere(" + lottery.getText().toString() + ")");
     }
 }
