06e5726e203d9ff38da5c718719a940287e52e6c
brad <brad@brad.tw>
2019-12-11 19:58:59 +0800

    4

--- app/src/main/java/tw/org/iii/android201910/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201910/MainActivity.java
@@ -3,6 +3,7 @@ package tw.org.iii.android201910;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private void initWebView(){
         WebViewClient client = new WebViewClient();
         webView.setWebViewClient(client);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
 
         webView.loadUrl("https://www.iii.org.tw");
     }
