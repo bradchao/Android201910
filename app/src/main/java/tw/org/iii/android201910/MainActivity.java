59af81f97fc256775d538804f28a4f871e784e49
brad <brad@brad.tw>
2019-12-11 19:36:26 +0800

    1

---
+++ app/src/main/java/tw/org/iii/android201910/MainActivity.java
@@ -0,0 +1,18 @@
+package tw.org.iii.android201910;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+    }
+}
